bb74ffc81a5ee4cf4f75c2193ec045a9bd26ad84
unknown <sagarmaan2015@gmail.com>
2022-12-21 18:18:21 +0530

    done

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,59 +1,58 @@
 const { count } = require("console")
 const BookModel= require("../models/bookModel")
+const AuthorModel = require("../models/authorModel")
+const { default: mongoose } = require("mongoose")
 
-const createBook= async function (req, res) {
-    let data= req.body
 
-    let savedData= await BookModel.create(data)
-    res.send({msg: savedData})
+// Write create APIs for both books and authors ---> If author_id is not available then do not accept the entry(in neither the author collection nor the books collection)
+
+
+exports.createAuthorData = async function ( req , res) {
+    let author = req.body  
+    let authorData = await AuthorModel.create(author)
+    
+    res.json({msg : authorData})  
+    
 }
 
-const getBooksData= async function (req, res) {
-    let allBooks= await BookModel.find( {authorName : "HO" } )
-    console.log(allBooks)
-    if (allBooks.length > 0 )  res.send({msg: allBooks, condition: true})
-    else res.send({msg: "No books found" , condition: false})
+exports.createBookData= async function (req, res) {
+    let {name, author_id, price, ratings} = req.body;
+       if(!author_id || !mongoose.Types.ObjectId.isValid(author_id)){
+              return res.json({msg:'please provide valid author id'})
+       }
+       let book = await BookModel.create({name,author_id,price,ratings});
+       res.json(book);
+}
+
+// List out the books written by "Chetan Bhagat" ( this will need 2 DB queries one after another- first query will find the author_id for "Chetan Bhagat”. Then next query will get the list of books with that author_id )
+
+
+exports.findauthorbyid=async function(req,res){
+    let author = await AuthorModel.findOne({author_name:"Chetan Bhagat"})
+    let book = await BookModel.find({author_id:author._id})
+    res.send({msg:book})
+}
+    
+// find the author of “Two states” and update the book price to 100;  Send back the author_name and updated price in response.  ( This will also need 2  queries- 1st will be a findOneAndUpdate. The second will be a find query aith author_id from previous query)
+
+exports.updatePrice = async function (req ,res) {
+    let update = await BookModel.findOneAndUpdate({name : "Two states"},{$set:{price:100}},{new:true})
+     let authors = await AuthorModel.findById(update.author_id)
+     res.send({price:update.price,author_name:authors.author_name})
+}
+
+// Find the books which costs between 50-100(50,100 inclusive) and respond back with the author names of respective books.. 
+// bookModel.find( { price : { $gte: 50}  ,  price: {$lte: 100} } ) // WRONG
+// bookModel.find( { price : { $gte: 50, $lte: 100} } ).select({ author_id :1})..run a map(or forEach) loop and get all the authorName corresponding to the authorId’s ( by querying authorModel)
+
+exports.bookPrice = async function(req,res){
+let price = await BookModel.find({price:{$gte:50 , $lte:100}}).select("author_id")
+let names = []
+for ( i=0;i<price.length;i++){
+    let newNames = await AuthorModel.findOne({_id:price[i].author_id}).select("author_name")
+    names.push(newNames)
+}
+res.send({msg:names})
 }
 
 
-const updateBooks= async function (req, res) {
-    let data = req.body // {sales: "1200"}
-    // let allBooks= await BookModel.updateMany( 
-    //     { author: "SK"} , //condition
-    //     { $set: data } //update in data
-    //  )
-    let allBooks= await BookModel.findOneAndUpdate( 
-        { authorName: "ABC"} , //condition
-        { $set: data }, //update in data
-        { new: true , upsert: true} ,// new: true - will give you back the updated document // Upsert: it finds and updates the document but if the doc is not found(i.e it does not exist) then it creates a new document i.e UPdate Or inSERT  
-     )
-     
-     res.send( { msg: allBooks})
-}
-
-const deleteBooks= async function (req, res) {
-    // let data = req.body 
-    let allBooks= await BookModel.updateMany( 
-        { authorName: "FI"} , //condition
-        { $set: {isDeleted: true} }, //update in data
-        { new: true } ,
-     )
-     
-     res.send( { msg: allBooks})
-}
-
-
-
-
-// CRUD OPERATIONS:
-// CREATE
-// READ
-// UPDATE
-// DELETE
-
-
-
-module.exports.createBook= createBook
-module.exports.getBooksData= getBooksData
-module.exports.updateBooks= updateBooks
-module.exports.deleteBooks= deleteBooks
--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
-    useNewUrlParser: true
+mongoose.connect("mongodb+srv://SagarMaan:yHJBlRWQ0FdJmdj6@chaudhary-shaab-db.cueddss.mongodb.net/Book", {
+    useNewUrlParser: true    
 })
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
@@ -17,6 +17,6 @@ mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzot
 app.use('/', route);
 
 
-app.listen(process.env.PORT || 3000, function () {
-    console.log('Express app running on port ' + (process.env.PORT || 3000))
+app.listen(process.env.PORT || 8000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 8000))
 });
