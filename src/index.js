bfc30baf6185c592c19b8872b6832bf3eb5d6fe6
unknown <sagarmaan2015@gmail.com>
2022-12-23 22:15:15 +0530

    done

--- src/index.js
+++ src/index.js
@@ -8,22 +8,33 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://SagarMaan:yHJBlRWQ0FdJmdj6@chaudhary-shaab-db.cueddss.mongodb.net/Middleware", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
-app.use (
-    function (req, res, next) {
-        console.log ("inside GLOBAL MW");
-        next();
-  }
-  );
+const firstMiddleWear = function (req, res, next) {
+    var currentdate = new Date(); 
+    var datetime =  currentdate.getDate() + " "
+                    + (currentdate.getMonth()+1)  + " " 
+                    + currentdate.getFullYear() + "  "  
+                    + currentdate.getHours() + ":"  
+                    + currentdate.getMinutes() + ":" 
+                    + currentdate.getSeconds();
+
+    let ip = req.ip
+    let url= req.originalUrl
+    console.log(`${datetime}  ${ip}  ${url}`)
+    next()    
+}
+
+app.use( firstMiddleWear)
+
 
 app.use('/', route);
 
 
-app.listen(process.env.PORT || 3000, function () {
-    console.log('Express app running on port ' + (process.env.PORT || 3000))
+app.listen(5000, function () {
+    console.log('Express app running on port ' + (5000))
 });
